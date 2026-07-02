234355f7124128e841b545be2eb8f6ecfa22c526
honeypie3301 <84184414+honeypie3301@users.noreply.github.com>
2026-07-02 19:48:03 +0800

    Update 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   return {
-    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
