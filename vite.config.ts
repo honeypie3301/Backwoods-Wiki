815ded3714fd64be5d2e00d587c89ee56518e62e
honeypie3301 <84184414+honeypie3301@users.noreply.github.com>
2026-08-08 20:21:12 +0800

    Vercel fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(({ command }) => {
   return {
-    base: command === 'build' ? '/Backwoods-Wiki/' : '/',
+    base: process.env.GITHUB_ACTIONS === 'true' ? '/Backwoods-Wiki/' : '/',
     publicDir: 'wiki_assets',
     plugins: [react(), tailwindcss()],
     resolve: {
